9b1cbc185d21311765087faf1fdc882bb5bf86df
VGligorijevic <vgligorijevic@flatironinstitute.org>
2019-12-22 18:15:01 -0500

    updated

--- cmap2TFRecord.py
+++ cmap2TFRecord.py
@@ -1,6 +1,7 @@
+import sys
 import pickle
 import numpy as np
-from deepfrier.utils import seq2onehot, load_catalogue
+from deepfrier.utils import seq2onehot, load_catalogue, norm_adj
 import tensorflow as tf
 
 
@@ -11,7 +12,7 @@ class GenerateTFRecord(object):
     def _dtype_feature(self):
         return lambda array: tf.train.Feature(int64_list=tf.train.Int64List(value=array))
 
-    def convert_numpy_folder(self, catalogue, ont_idx, tfrecord_fn):
+    def convert_numpy_folder(self, catalogue, tfrecord_fn):
         writer = tf.python_io.TFRecordWriter(tfrecord_fn)
         print ("Serializing {:d} examples into {}".format(len(self.labels), tfrecord_fn))
 
@@ -20,7 +21,12 @@ class GenerateTFRecord(object):
             cmap = np.load(catalogue[chain])
             A_ca = cmap['A_ca_10A']
             A_all = cmap['A_all_6A']
-            A_nbr = cmap['A_nbr']
+            if 'A_nbr' in cmap:
+                A_nbr = cmap['A_nbr']
+            elif 'A_nbr_bin' in cmap:
+                A_nbr = norm_adj(cmap['A_nbr_bin'])
+            else:
+                sys.exit("NBR contact maps not in the *npz file!")
             S = seq2onehot(str(cmap['sequence']))
             L = cmap['L'].item()
             y = np.asarray(self.labels[chain], dtype=np.int64)
@@ -32,9 +38,7 @@ class GenerateTFRecord(object):
             d_feature['A_all'] = tf.train.Feature(float_list=tf.train.FloatList(value=A_all.reshape(-1)))
             d_feature['S'] = tf.train.Feature(float_list=tf.train.FloatList(value=S.reshape(-1)))
 
-            d_feature['mf_labels'] = dtype_feature_y(y[ont_idx[0]])
-            d_feature['bp_labels'] = dtype_feature_y(y[ont_idx[1]])
-            d_feature['cc_labels'] = dtype_feature_y(y[ont_idx[2]])
+            d_feature['labels'] = dtype_feature_y(y)
             d_feature['L'] = tf.train.Feature(int64_list=tf.train.Int64List(value=[L]))
 
             features = tf.train.Features(feature=d_feature)
@@ -44,37 +48,35 @@ class GenerateTFRecord(object):
 
 
 if __name__ == "__main__":
-    cmap2path = load_catalogue(fn='/mnt/ceph/users/vgligorijevic/ContactMaps/data/nr_pdb_chains/catalogue.csv')
-    # cmap2path = load_catalogue()
-    out_path = '/mnt/ceph/users/vgligorijevic/ContactMaps/data/TFRecord/'
-    for seqid in [30]:
-        path = '/mnt/home/vgligorijevic/Projects/NewMethods/Contact_maps/go_annot/merged_ont/pdb_GO_train_test_split_bc_' + str(seqid) + '.pckl'
-        # path = '/mnt/ceph/users/vgligorijevic/ContactMaps/data/SIFTS_EC/pdb_EC_train_test_split_bc_' + str(seqid) + '.pckl'
-        # path = '/mnt/ceph/users/vgligorijevic/ContactMaps/data/Swiss-Model/swiss-model_EC_train_test_split.pckl'
-        annot = pickle.load(open(path, 'rb'))
-        ontology = annot['ontology']
-        mf_idx = np.where(ontology == 'molecular_function')[0]
-        bp_idx = np.where(ontology == 'biological_process')[0]
-        cc_idx = np.where(ontology == 'cellular_component')[0]
-        # for split in ['train', 'valid']:
-        for split in ['valid']:
-            chains = annot[split + '_pdb_chains']
-            Y = annot['Y_' + split]
-            # for num, evid in enumerate(['EXP', 'EXP-IEA']):
-            for num, evid in enumerate(['EXP']):
-                print ('pdb_chains_seqid_' + str(seqid) + '_' + split + '_' + evid + '.tfrecords')
-                tmp_Y = Y.copy()
-                tmp_chains = chains.copy()
+    cmap2path = load_catalogue(fn='/mnt/ceph/users/vgligorijevic/ContactMaps/data/Swiss-Model/merged_annot/catalogue.csv')
+    out_path = '/mnt/ceph/users/vgligorijevic/ContactMaps/data/Swiss-Model/merged_annot/tfrecords/'
+    pckl_fn = '/mnt/ceph/users/vgligorijevic/ContactMaps/data/Swiss-Model/merged_annot/merged_GO_train_test_split_nr30.pckl'
+    num = {'EXP': 0, 'EXP-IEA': 1}
 
-                tmp_Y[tmp_Y == -1] = num
-                idx = np.where(tmp_Y.sum(axis=1) > 0)[0]
-                tmp_Y = tmp_Y[idx]
-                tmp_chains = tmp_chains[idx]
+    split = str(sys.argv[1])
+    evid = str(sys.argv[2])
+    cmap = str(sys.argv[3])
+    ont = str(sys.argv[4])
+    begin = int(sys.argv[5])
+    end = int(sys.argv[6])
 
-                labels = {}
-                for i, chain in enumerate(tmp_chains):
-                    labels[chain] = tmp_Y[i]
-                tfr = GenerateTFRecord(labels)
-                tfr.convert_numpy_folder(cmap2path, [mf_idx, bp_idx, cc_idx],
-                                         out_path + 'pdb_chains_seqid_' + str(seqid) + '_' + split + '_' + evid + '.tfrecords_')
-                # tfr.convert_numpy_folder(cmap2path, out_path + 'swiss-model_chains_seqid_90_' + ont + '_' + split + '_EC.tfrecords')
+    annot = pickle.load(open(pckl_fn, 'rb'))
+    ontology = annot['ontology']
+    Y = annot['Y_' + evid].toarray()
+    go_idx = np.where(ontology == ont)[0]
+    Y = Y[:, go_idx]
+    Y[Y == -1] = num[evid]
+
+    chains = annot[split + '_pdb_chains']
+    cmap_type = annot[split + '_cmap_type']
+    idx = np.where(cmap_type == cmap)[0]
+    chains = chains[idx]
+    Y = Y[idx]
+
+    labels = {}
+    for i, chain in enumerate(chains):
+        labels[chain] = Y[i]
+
+    tfr = GenerateTFRecord(labels)
+    tfr.convert_numpy_folder(cmap2path,
+                             out_path + cmap + '_chains_' + ont + '_seqid_30_' + split + '_' + evid + '_' + str(begin) + '-' + str(end) + '.tfrecords')
